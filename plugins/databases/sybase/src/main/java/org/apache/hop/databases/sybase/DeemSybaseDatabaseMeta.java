7fbd3a1e0fed6d8ab978e8d0f08f4624c101cb30
fskorgen <frank@skorgen.no>
2023-12-17 09:50:35 +0100

    DEEM-MOD Sybase

---
+++ plugins/databases/sybase/src/main/java/org/apache/hop/databases/sybase/DeemSybaseDatabaseMeta.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hop.databases.sybase;
+
+import org.apache.hop.core.database.DatabaseMeta;
+import org.apache.hop.core.database.DatabaseMetaPlugin;
+
+@DatabaseMetaPlugin(type = "DEEM_SYBASE", typeDescription = "Deem Sybase")
+public class DeemSybaseDatabaseMeta extends SybaseDatabaseMeta {
+
+  @Override
+  public int[] getAccessTypeList() {
+    return new int[] {DatabaseMeta.TYPE_ACCESS_NATIVE};
+  }
+
+  @Override
+  public int getDefaultDatabasePort() {
+    if (getAccessType() == DatabaseMeta.TYPE_ACCESS_NATIVE) {
+      return 2638; // Slettvoll:8095
+    }
+    return -1;
+  }
+
+  @Override
+  public String getDriverClass() {
+    return "sap.jdbc4.sqlanywhere.IDriver";
+  }
+
+  @Override
+  public String getURL(String hostname, String port, String databaseName) {
+    return "jdbc:sqlanywhere:eng=" + databaseName + ";database=" + databaseName + ";links=tcpip(host=" + hostname + ":" + port + ")";
+  }
+
+}
